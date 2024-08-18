e260a4ece1aec306434a162d35ffb459ab0be046
abenomran <abdalla.benomran2@gmail.com>
2024-08-18 13:11:41 -0400

    is anagram

---
+++ Is Anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        charStack = {}
+        charStack2 = {}
+        for c in s:
+            charStack[c] = 1 + charStack.get(c, 0)
+        for c in t:
+            charStack2[c] = 1 + charStack2.get(c, 0)
+
+        return charStack == charStack2
+        

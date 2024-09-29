8eb37b11b82e280c8fc3c013b97219f49709b6bb
abenomran <abdalla.benomran2@gmail.com>
2024-09-29 10:59:24 -0400

    subtree of a binary tree

---
+++ Subtree of a Binary Tree.py
@@ -0,0 +1,31 @@
+# Easy
+from typing import Optional, TreeNode
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        elif not root:
+            return False
+        
+        if self.isEqual(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isEqual(self, p, q):
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isEqual(p.left, q.left) and self.isEqual(p.right, q.right)

319371a31a7782434d563284357c47982c23152c
abenomran <abdalla.benomran2@gmail.com>
2024-10-02 19:10:55 -0400

    string encode and decode

---
+++ String Encode and Decode.py
@@ -0,0 +1,23 @@
+# Medium
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encodedStr = ""
+        for s in strs:
+            encodedStr += str(len(s)) + "#" + s
+        return encodedStr
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            i = j + length + 1
+            res.append(s[j + 1: i])
+            
+        return res

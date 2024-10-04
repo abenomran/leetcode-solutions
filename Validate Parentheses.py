fbc497ed9ae78db703c322073e483169258d53c5
abenomran <abdalla.benomran2@gmail.com>
2024-10-03 20:28:59 -0400

    valid parentheses

--- Validate Parentheses.py
+++ Validate Parentheses.py
@@ -2,15 +2,14 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        pMap = {")": "(", "]": "[", "}": "{"}
         stack = []
-        opDict = {"]":"[", "}":"{", ")":"("}
-
         for c in s:
-            if c in opDict:
-                if not stack or stack[-1] != opDict[c]:
-                    return False
-                stack.pop()
-            else:
-                stack.append(c)
+            if c in pMap:
+                if stack and stack[-1] == pMap[c]:
+                    stack.pop()
+                    continue
+                return False
+            stack.append(c)
 
         return not stack

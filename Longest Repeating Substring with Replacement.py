41849d0cf8232effe7c6c61ef675cf4b763bc568
abenomran <abdalla.benomran2@gmail.com>
2024-10-06 19:54:37 -0400

    longest repeating substring with replacement

--- Longest Repeating Substring with Replacement.py
+++ Longest Repeating Substring with Replacement.py
@@ -3,15 +3,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         L = 0
-        cHash = {}
-        maxC = 0
+        count = {}
+        maxFreq = 0
 
         for R in range(len(s)):
-            cHash[s[R]] = 1 + cHash.get(s[R], 0)
-            maxC = max(maxC, cHash[s[R]])
-            
-            if (R - L + 1) - maxC > k:
-                cHash[s[L]] -= 1
+            count[s[R]] = 1 + count.get(s[R], 0)
+            maxFreq = max(maxFreq, count[s[R]])
+            if (R - L + 1) - maxFreq > k:
+                count[s[L]] -= 1
                 L += 1
 
         return (R - L + 1)

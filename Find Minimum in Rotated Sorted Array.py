27512ff4502809df6612017d28b592042633c2d2
abenomran <abdalla.benomran2@gmail.com>
2024-10-06 18:39:11 -0400

    find minimum in rotated sorted array

--- Find Minimum in Rotated Sorted Array.py
+++ Find Minimum in Rotated Sorted Array.py
@@ -4,15 +4,17 @@ from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
+        res = nums[0]
 
         while l < r:
-            m = l + (r - l) // 2
+            m = l + ((r - l) // 2)
+            res = min(res, nums[m])
 
             if nums[m] > nums[r]:
                 l = m + 1
             else:
-                r = m
-
-        return nums[l]
+                r = m - 1
+            
+        return min(res, nums[l])
 
             

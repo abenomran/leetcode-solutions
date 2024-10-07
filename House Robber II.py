af183099ea89bd790c0e96c45ec2944e39f21bb3
abenomran <abdalla.benomran2@gmail.com>
2024-10-06 22:41:59 -0400

    house robber 2

--- House Robber II.py
+++ House Robber II.py
@@ -1,6 +1,8 @@
 # Medium
 from typing import List
 
+"""
+# Initial Solution
 class Solution:
     def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
@@ -17,3 +19,20 @@ class Solution:
             rob4 = tmp
 
         return max(rob4, rob2, nums[0])
+"""
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[:-1]), self.helper(nums[1:]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            tmp = max(rob2, rob1 + n)
+            rob1 = rob2
+            rob2 = tmp
+        
+        return rob2
+
+        

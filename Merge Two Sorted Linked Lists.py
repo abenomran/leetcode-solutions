bc2d1410afc9e9985e5caed1ee95cd20bc953f90
abenomran <abdalla.benomran2@gmail.com>
2024-10-10 00:10:39 -0400

    merge two sorted linked lists

--- Merge Two Sorted Linked Lists.py
+++ Merge Two Sorted Linked Lists.py
@@ -7,23 +7,30 @@ from typing import Optional, ListNode
 #         self.val = val
 #         self.next = next
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr = ListNode(0, None)
-        newList = curr
-        
+        dummy = ListNode()
+        curr = dummy
+
         while list1 and list2:
-            if list1.val <= list2.val:
-                curr.next = ListNode(list1.val, None)
-                list1 = list1.next
-            else:
-                curr.next = ListNode(list2.val, None)
+            if list2.val <= list1.val:
+                curr.next = list2
                 list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
             curr = curr.next
-            
+
         if list1:
             curr.next = list1
         if list2:
             curr.next = list2
-        
-        return newList.next
+
+        return dummy.next
+                            

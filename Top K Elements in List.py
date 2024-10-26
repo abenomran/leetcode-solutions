caf32357cd100f9e9bec780a16bfcd11643c33e4
abenomran <abdalla.benomran2@gmail.com>
2024-10-26 15:42:48 -0400

    top k elements in list

--- Top K Elements in List.py
+++ Top K Elements in List.py
@@ -4,19 +4,17 @@ from typing import List
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = {}
-        freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
             counts[n] = 1 + counts.get(n, 0)
 
-        for num, count in counts.items():
-            freq[count].append(num)
-        
+        freq = [[] for i in range(len(nums) + 1)]
+        for n, count in counts.items():
+            freq[count].append(n)
+
         res = []
         for i in range(len(freq) - 1, 0, -1):
-            numList = freq[i]
-            for num in numList:
-                res.append(num)
+            for n in freq[i]:
+                res.append(n)
                 if len(res) == k:
                     return res
-
